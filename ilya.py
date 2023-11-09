59dca4a84a8de5ed14bdde55e3233a0af1b883e0
morozovkm <morozovkm@yandex.ru>
2023-11-09 20:30:08 +0300

    вставка данных в бд из файла!

---
+++ ilya.py
@@ -0,0 +1,18 @@
+import pymysql
+
+f = open("users.txt", "r")
+connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
+cursor = connection.cursor()
+
+for line in f:
+    line = line.strip()
+    split_line = line.split(":")
+    name = split_line[0]
+    surname = split_line[1]
+    sex = split_line[2]
+    phone = split_line[3]
+    #print(f'insert into telsprav values("{name}","{surname}","{sex}","{phone}")')
+    cursor.execute(f'insert into telsprav values("{name}","{surname}","{sex}","{phone}")')
+f.close()
+
+connection.commit()
