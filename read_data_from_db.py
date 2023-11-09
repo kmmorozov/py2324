77d366ba601cd3cd0deed9faf6f74ce7a0f0127a
morozovkm <morozovkm@yandex.ru>
2023-11-09 21:08:20 +0300

    чтение из БД и запись в файл

---
+++ read_data_from_db.py
@@ -0,0 +1,22 @@
+import pymysql
+
+connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
+cursor = connection.cursor()
+
+cursor.execute('select * from telsprav')
+
+data = cursor.fetchall()
+
+for elem in data:
+    print(elem)
+    name = elem[0]
+    surname = elem[1]
+    sex = elem[2]
+    phone = elem[3]
+
+    print(name , phone)
+
+
+
+
+
