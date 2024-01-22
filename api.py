e8641338636e3f0db03ee5ba5693659ac291ae33
morozovkm <morozovkm@yandex.ru>
2024-01-22 19:30:31 +0300

    Занятие 202401221

--- api.py
+++ api.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
 import datetime
+import random
+
 @app.get("/")
 async def myfync():
     return datetime.datetime.now()
 
 @app.get("/test1")
 async def myfync():
-    return "kirill"
+    number = random.randint(0,1000000)
+    return str(number)
